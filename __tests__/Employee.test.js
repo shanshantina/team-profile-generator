be910ae5f355e349f4436e211c9239546f0f3768
Shanshan Xu <tinaxu84@gmail.com>
2021-02-06 13:34:32 -0500

    add code for employee test and employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,43 @@
+const { expect, test } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
-test("Can instantiate Employee instance", () => {
+test('create an employee object', () => {
   const employee = new Employee();
   expect(typeof(employee)).toBe("object");
 });
 
+test("set the employee's name", () => {
+    const employee = new Employee('Dave');
+    expect(employee.name).toBe('Dave');
+});
+
+test("set the employee's id", () => {
+    const employee = new Employee('Dave', 100);
+    expect(employee.id).toBe(100);
+});
+
+test("set the employee's email", () => {
+    const employee = new Employee('Dave', 1, "test@test.com");
+    expect(employee.email).toBe("test@test.com");
+});
+
+test("get the employee's name", () => {
+    const employee = new Employee('Dave');
+    expect(employee.getName()).toBe('Dave');
+});
+
+test("get the employee's id", () => {
+    const employee = new Employee('Dave', 100);
+    expect(employee.getId()).toBe(100);
+});
+
+test("get the employee's email", () => {
+    const employee = new Employee('Dave', 1, "test@test.com");
+    expect(employee.getEmail()).toBe("test@test.com");
+});
+
+test("get the employee's role to return 'Employee'", () => {
+    const testRole = "Employee";
+    const employee = new Employee('Dave', 1, "test@test.com");
+    expect(employee.getRole()).toBe(testRole);
+})
